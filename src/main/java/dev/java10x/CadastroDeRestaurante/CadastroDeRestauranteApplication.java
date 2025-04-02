cb204b34c8ff490066fc86214396b29b6193a761
Thiago <thiago.silveira@g4it.com.br>
2025-04-02 18:45:14 -0300

    primeiro commit

---
+++ src/main/java/dev/java10x/CadastroDeRestaurante/CadastroDeRestauranteApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeRestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeRestauranteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/CadastroDeRestaurante/CadastroDeRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeRestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
