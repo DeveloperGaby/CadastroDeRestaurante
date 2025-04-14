6d6befc6c00b31eb23175d9854c2321f60036764
Thiago <thiago.silveira@g4it.com.br>
2025-04-14 16:09:46 -0300

    feat: Inclui Lombok na classe para abstracao

--- src/main/java/dev/java10x/CadastroDeRestaurante/Cardapios/CardapiosModel.java
+++ src/main/java/dev/java10x/CadastroDeRestaurante/Cardapios/CardapiosModel.java
@@ -2,11 +2,17 @@ package dev.java10x.CadastroDeRestaurante.Cardapios;
 
 import dev.java10x.CadastroDeRestaurante.Restaurante.Controller.RestauranteModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name="tb_cardapios")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class CardapiosModel {
 
     @Id
--- src/main/java/dev/java10x/CadastroDeRestaurante/Restaurante/Controller/RestauranteModel.java
+++ src/main/java/dev/java10x/CadastroDeRestaurante/Restaurante/Controller/RestauranteModel.java
@@ -2,6 +2,9 @@ package dev.java10x.CadastroDeRestaurante.Restaurante.Controller;
 
 import dev.java10x.CadastroDeRestaurante.Cardapios.CardapiosModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -9,46 +12,21 @@ import java.util.List;
 // JPA java persistence API
 @Entity
 @Table(name="tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class RestauranteModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private Long id;
+    private Long id;
     private String nome;
     private String email;
 
     //@ManyToOne um restaurante pode ter varios cardapios
     @ManyToOne
-    @JoinColumn(name="cardapios_id")// Foreing key ou chave estrangeira
+    @JoinColumn(name = "cardapios_id")// Foreing key ou chave estrangeira
     private CardapiosModel cardapios;
-
-
-    public RestauranteModel() {
-    }
-
-
-    public RestauranteModel(String nome, String email) {
-        this.nome = nome;
-        this.email = email;
-    }
-
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
 
 
 }
